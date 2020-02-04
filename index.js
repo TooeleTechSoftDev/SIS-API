3828a4f75bc77ef9b1af6a1371863d801dafe661
Iskarr <32428946+Iskarr@users.noreply.github.com>
2020-02-04 15:16:27 -0700

    added mongodb to index.js file, and changed console.log color.

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
+var http = require("http").Server(app);
 
+// Mongodb connection:
+// Must add login and pw
+const db = require("./db");
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
-var http = require('http').Server(app)
-
-const jwt = require('./helpers/jwt')
-
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
-app.use(cors()) 
+const jwt = require("./helpers/jwt");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 //app.use(jwt())
 
 //Event Feed
-app.use('/events',require('./events/events.controller'))
+app.use("/events", require("./events/events.controller"));
 //app.use('/staff', require('./staff/staff.controller'))
 //app.use('/student', require('./student/student.controller'))
 
-const port = 8088
-http.listen(port, () => {console.log("Listening on port ",port)})
+const port = 8088;
+http.listen(port, () => {
+  console.log("\x1b[33m%s\x1b[0m", "Listening on port ", port);
+});
